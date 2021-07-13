0da271748e85721aac448821707c9ea844a9184d
Tyler Bratton <tylerbratton96@gmail.com>
2021-07-12 20:55:27 -0400

    Add contact email and adjust flex styling

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <img
-      class="h-16 w-16 md:h-32 md:w-32 lg:h-48 lg:w-48"
-      id="logo"
-      alt="Petrichor Logo"
-      src="./assets/logo.png"
-  >
+  <div class="flex flex-col items-center">
+    <img
+        class="h-16 w-16 md:h-32 md:w-32 lg:h-48 lg:w-48"
+        id="logo"
+        alt="Petrichor Logo"
+        src="./assets/logo.png"
+    >
+    <a class="text-white m-4" href="mailto:contact@petrichor.band">
+      contact@petrichor.band
+    </a>
+  </div>
   <HelloWorld/>
 </template>
 
